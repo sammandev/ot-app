5c287c484af05590ee878b5c32b0b243ba16fc5d
GitHub_CopilotE01 <samhaloman.dev@gmail.com>
2026-04-06 14:56:56 +0700

    feat: add migration to merge notification target data and remove custom recipients

---
+++ backend/api/migrations/0059_merge_0057_notification_target_data_and_0058.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("api", "0057_notification_target_data"),
+        ("api", "0058_remove_systemconfiguration_leave_notification_custom_recipients"),
+    ]
+
+    operations = []
